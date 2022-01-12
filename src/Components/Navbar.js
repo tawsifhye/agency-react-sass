2d7111c2c8b45770a47a786b3cb75a67a030b40d
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-12 15:38:21 +0600

    rspomsive navbar added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Navbar.scss'
+import Navlink from './Navlink';
 const Navbar = () => {
+    const [togglerClicked, setTogglerClicked] = useState(false);
+
     return (
         <>
             <div className='navbar'>
                 <div className='logo'>
                     Agency
                 </div>
-                <div className="nav-link">
-                    <a href="http://">Home</a>
-                    <a href="http://">Services</a>
-                    <a href="http://">Our Projects</a>
-                    <a href="http://">About Us</a>
-                </div>
-                <a className='button' href="http://">Contact Us</a>
+                {
+                    window.innerWidth > 768 ?
+                        <>
+                            <div className="nav-link">
+                                <a className='link' href="http://">Home</a>
+                                <a className='link' href="http://">Services</a>
+                                <a className='link' href="http://">Our Projects</a>
+                                <a className='link' href="http://">About Us</a>
+                            </div>
+                            <a className='button' href="http://">Contact Us</a>
+                        </>
+                        :
+                        <div className='burger-menu'>
+                            {
+                                !togglerClicked ? <i className="fas fa-bars" onClick={() => setTogglerClicked(true)}></i>
+                                    :
+                                    <i class="far fa-minus-square" onClick={() => setTogglerClicked(false)}></i>
+                            }
+                        </div>
+                }
+
+                {
+                    togglerClicked && <div className="mobile-menu">
+                        <a href="http://">Home</a>
+                        <a href="http://">Services</a>
+                        <a href="http://">Our Projects</a>
+                        <a href="http://">About Us</a>
+                        <a className='button' href="http://">Contact Us</a>
+                    </div>
+                }
+
             </div>
         </>
     );
---
+++ src/Components/Navlink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/Navbar.scss'
+const Navlink = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Navlink;
