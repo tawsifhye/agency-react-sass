819487e279e65732a227b2126fb21dff87754814
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-13 10:28:11 +0600

    button added to HEro section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BiRightArrowAlt } from "react-icons/bi";
 import '../Styles/Hero.scss'
 import banner from '../images/hero-image.png'
 const Hero = () => {
@@ -11,6 +12,7 @@ const Hero = () => {
                 <p>
                     Enver studio is a digital studio that offers several services such as UI/UX Design to developers, we will provide the best service for those of you who use our services.
                 </p>
+                <a className="button" href="https://">Our Services <BiRightArrowAlt className='arrow' /></a>
             </div>
             <div className='hero-banner'>
                 <img src={banner} alt="" />
