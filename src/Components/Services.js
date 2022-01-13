556198a32570f641128bb1403ec692ddae09ccc5
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-13 14:18:46 +0600

    service section added

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -6,33 +6,39 @@ import SingleService from './SingleService';
 const services = [
     {
         'id': '1',
+        'icon': 'fas fa-laptop-code',
         'title': 'Development',
         'details': 'Create a platform with the best and coolest quality from us.',
     },
     {
         'id': '2',
+        'icon': 'fas fa-layer-group',
         'title': 'UI/UX Designer',
         'details': 'We provide UI/UX Design services, and of course with the best quality',
     },
     {
         'id': '3',
+        'icon': 'fas fa-layer-group',
         'title': 'Graphik Designer',
         'details': 'Create a platform with the best and coolest quality from us.',
     },
     {
         'id': '4',
+        'icon': 'fas fa-laptop-code',
         'title': 'Motion Graphik',
         'details': 'We provide Graphic Design services, with the best designers',
     },
 
     {
         'id': '5',
+        'icon': 'fas fa-laptop-code',
         'title': 'Photography',
         'details': 'We provide Photography services, and of course with the best quality',
     },
 
     {
         'id': '6',
+        'icon': 'fas fa-laptop-code',
         'title': 'Videography',
         'details': 'Create a platform with the best and coolest quality from us.',
     },
--- src/Components/SingleService.js
+++ src/Components/SingleService.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../Styles/SingleService.scss';
 const SingleService = ({ service }) => {
+    console.log(service.icon);
     return (
         <div className="single-service">
+            <i className={service.icon}></i>
             <div className="service-title">{service.title}</div>
             <div className="service-details">{service.details}</div>
         </div>
